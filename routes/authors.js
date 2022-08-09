73f896f7eee7dc330aa98ec07eb67b80c176ae21
Vermaak van Wyk <vermaakvanw@fmi.co.za>
2022-08-09 16:48:09 +0200

    Initial authers routes

---
+++ routes/authors.js
@@ -0,0 +1,52 @@
+const app = require("express");
+const Author = require("../models/author");
+const router = app.Router();
+
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name != "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+router.post("/new", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    //res.redirect(`/authors/${newAuthor.id}`);
+    res.redirect("/authors");
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating error",
+    });
+  }
+  //   author.save((err, newAuthor) => {
+  //     if (err) {
+  //       res.render("authors/new", {
+  //         author: author,
+  //         errorMessage: "Error creating error",
+  //       });
+  //     } else {
+  //       //res.redirect(`/authors/${newAuthor.id}`);
+  //       res.redirect("/authors");
+  //     }
+  //   });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -14,6 +17,7 @@ app.set("layout", __dirname + "/layouts/layout");
 
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -22,5 +26,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to MongoDb"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 app.listen(process.env.PORT || 3000);
