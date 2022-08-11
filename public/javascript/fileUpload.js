d746e9ad5dc8dfd8e697e7f340ff7f6e65e31e9d
Vermaak van Wyk <vermaakvanw@fmi.co.za>
2022-08-11 07:35:44 +0200

    completed styling

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,13 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-    FilePondPluginFileValidateType
-);
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    targetWidth: 100,
-    targetHeight: 150,
-    acceptedFileTypes: ['image/*'],
-});
-FilePond.parse(document.body);
+const rootStyles = window.getComputedStyle(document.documentElement);
+
+if (
+    rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+    rootStyles.getPropertyValue('--book-cover-width-large') != ''
+) {
+    ready();
+} else {
+    document.getElementById('main-css').addEventListener('load', ready);
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));
+    const aspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = parseFloat(coverWidth / aspectRatio);
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+        FilePondPluginFileValidateType
+    );
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / aspectRatio,
+        targetWidth: coverWidth,
+        targetHeight: coverHeight,
+        acceptedFileTypes: ['image/*'],
+    });
+    FilePond.parse(document.body);
+}
